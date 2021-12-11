262bc22da03ec5fc1022c750187f25d51375ff75
Ewen <ewenelain@hotmail.fr>
2021-12-11 22:37:24 +0000

    Add skybox rotation effect on main menu

---
+++ Assets/Scripts/CameraRotationEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotationEffect : MonoBehaviour
+{
+    public float RotationSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, RotationSpeed * Time.deltaTime);
+    }
+}
